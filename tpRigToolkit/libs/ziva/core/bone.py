5e3faca6f7f967f08df37073648938587de954cc
Tomas Poveda <tpovedatd@gmail.com>
2020-10-13 04:03:21 +0200

    Added new functions related with Ziva bones and tissues

---
+++ tpRigToolkit/libs/ziva/core/bone.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains functions to workf with Ziva bones
+"""
+
+from __future__ import print_function, division, absolute_import
+
+import tpDcc.dccs.maya as maya
+
+
+def is_bone(mesh):
+    """
+    Returns whether or not given mesh is a Ziva Bone mesh or not
+    :param mesh: str, name of a mesh node
+    :return: bool
+    """
+
+    maya.cmds.select(clear=True)
+    ziva_tissue = maya.mel.eval('zQuery -type "zBone"')
+    if not ziva_tissue:
+        return False
+
+    return True
---
+++ tpRigToolkit/libs/ziva/core/tissue.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains functions to workf with Ziva tissues
+"""
+
+from __future__ import print_function, division, absolute_import
+
+import tpDcc.dccs.maya as maya
+
+
+def is_tissue(mesh):
+    """
+    Returns whether or not given mesh is a Ziva Tissue mesh or not
+    :param mesh: str, name of a mesh node
+    :return: bool
+    """
+
+    maya.cmds.select(clear=True)
+    ziva_tissue = maya.mel.eval('zQuery -type "zTissue"')
+    if not ziva_tissue:
+        return False
+
+    return True
